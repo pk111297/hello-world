a9c255b74e5be4e39e5cfef4a4b043bd8857b870
Pratik Kanungo <pratikkanungo111297@gmail.com>
2025-05-10 18:09:50 +0530

    React Tutorials

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Pratik</h1>
+// }
+
+const Greet = () => <h1>Hello Pratik from arrow function</h1>
+
+export default Greet;
